f34b6cdb48c7bf248d8092cb607d52c5c5e02905
Eric Timmerman <estimmerman@gmail.com>
2018-12-10 15:02:43 -0500

    scores for buckets

---
+++ Assets/Scripts/BucketScoreTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BucketScoreTrigger : MonoBehaviour {
+
+    public Text scoreText;
+    public int scoreValue;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("GenericMarble"))
+        {
+            scoreText.text = (int.Parse(scoreText.text) + scoreValue).ToString();
+        }
+    }
+}
